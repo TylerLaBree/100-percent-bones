14f29fd914f8a20f21562466fc838bb8ed9fd3d0
Tyler LaBree <tyler.labree@gmail.com>
2020-02-15 15:44:01 -0500

    moved and changed data reducer, now a function

---
+++ src/reduce_data.py
@@ -0,0 +1,28 @@
+from astropy.table import Table, vstack
+import os
+
+
+def clean(year_lim):
+    table4 = Table()
+    for file in os.listdir("../data"):
+        if file.endswith(".csv") & file.startswith("Surf"):
+            file = os.path.join("../data", file)
+            table1 = Table.read(file)
+            date = table1['yyyy-mm-ddThh:mm']
+            lat = table1['Latitude [degrees_north]']
+            long = table1['Longitude [degrees_east]']
+            temp = table1['TEMP [deg C]']
+            year = list()
+            for elt in date:
+                year.append(int(elt[0:4]))
+
+            table2 = Table()
+            table2.add_column(year, name='year')
+            table2.add_column(lat, name='lat')
+            table2.add_column(long, name='long')
+            table2.add_column(temp, name='temp')
+
+            table3 = table2[(table2['year'] >= year_lim) & (table2['temp'] > 0)]
+            table4 = vstack([table4, table3])
+
+    table4.write('../data/clean_data.csv', format='csv', overwrite=True)
