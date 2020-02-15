d5f0d0cf8428851c9eca814c89877dcdf48a3102
Tyler LaBree <tyler.labree@gmail.com>
2020-02-15 15:44:32 -0500

    moved and changed temp_map, now functions and includes visualization

--- src/temp_map.py
+++ src/temp_map.py
@@ -1,34 +1,42 @@
 from astropy.table import Table
 import numpy as np
+import seaborn as sb
+import matplotlib.pyplot as plt
 
-file = '../data/clean_data.csv'
-data = Table.read(file)
-data_table = data
 
-num_lat = 20
-num_long = 30
+def temp_vis(zoom_x, zoom_y):
+    tmap = np.genfromtxt("../data/tmap.csv",delimiter=',')
+    sb.set(font_scale=1)
+    binsx=np.linspace(-20, 9, 30*zoom_x)
+    binsx=binsx.astype(int)
+    binsy=np.linspace(69, 50, 20*zoom_y)
+    binsy=binsy.astype(int)
+    heat_map = sb.heatmap(tmap, xticklabels=binsx, yticklabels=binsy, cmap='coolwarm')
+    plt.show()
 
-npts = [[0 for x in range(num_long)] for y in range(num_lat)]
-map = [[0 for x in range(num_long)] for y in range(num_lat)]
 
-for i in range(num_lat,0,-1):
-    for j in range(0, num_long):
-        min_lat = i+50
-        min_long = j-20
+def temp_arr(zoom_x, zoom_y):
+    file = '../data/clean_data.csv'
+    data = Table.read(file)
+    data_table = data
 
-        for d_pt in data_table:
-            if min_lat < d_pt['lat'] < min_lat+1 and min_long < d_pt['long'] < min_long+1:
-                npts[20-i][j] += 1
-                map[20-i][j] += d_pt['temp']
+    num_lat = 20
+    num_long = 30
 
-for i in range(0, num_lat):
-    for j in range(0, num_long):
-        if npts[i][j] != 0:
-            map[i][j] /= npts[i][j]
-            npts[i][j] = np.math.floor(npts[i][j])
+    npts = [[0 for x in range(num_long*zoom_x)] for y in range(num_lat*zoom_y)]
+    tmap = [[0 for x in range(num_long*zoom_x)] for y in range(num_lat*zoom_y)]
 
-print(npts)
-print(map)
+    for d_pt in data_table:
+        i = zoom_y*50 - int(np.math.floor(d_pt['lat']*zoom_y))
+        j = int(np.math.floor(d_pt['long']*zoom_x)) + 20*zoom_x
+        npts[i][j] += 1
+        tmap[i][j] += d_pt['temp']
 
-np.savetxt('../data/npts.csv', npts, delimiter=',')
-np.savetxt('../data/tmap.csv', map, delimiter=',')
+    for i in range(0, num_lat*zoom_y):
+        for j in range(0, num_long*zoom_x):
+            if npts[i][j] != 0:
+                tmap[i][j] /= npts[i][j]
+                npts[i][j] = np.math.floor(npts[i][j])
+
+    np.savetxt('../data/npts.csv', npts, delimiter=',')
+    np.savetxt('../data/tmap.csv', tmap, delimiter=',')
