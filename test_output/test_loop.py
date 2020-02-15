b09ed2137a4cf9b4f1243feb6966fd6c2ae6173d
Emily Frame <eframe3@gmail.com>
2020-02-14 19:27:36 -0500

    filled in skeleton loop - now works for all data

--- test_output/test_loop.py
+++ test_output/test_loop.py
@@ -1,20 +1,30 @@
 from astropy.table import Table
 import numpy as np
 
-npts = []
-map = []
+file = '../data/clean_data.csv'
+data = Table.read(file)
+data_table = data[0:5]
 
-num_lat = 20
-num_long = 20
+num_lat = 1
+num_long = 1
+
+npts = [[0 for x in range(num_lat)] for y in range(num_long)]
+map = [[0 for x in range(num_lat)] for y in range(num_long)]
 
 for i in range(0,num_lat):
     for j in range(0,num_long):
-        min_lat = i-20
-        min_long = j-20
-        for d_pt in data:
-            if (d_pt['lat'] > min_lat & d_pt['lat'] < min_lat+1 & d_pt['long'] > min_long & d_pt['long'] < min_long+1):
-                npts[i,j]++
-                map[i,j] += d_pt['temp']
+        min_lat = i+50
+        min_long = j-2
+
+        for d_pt in data_table:
+            if (d_pt['lat'] > min_lat and d_pt['lat'] < min_lat+1 and d_pt['long'] > min_long and d_pt['long'] < min_long+1):
+                npts[i][j]+= 1
+                map[i][j] += d_pt['temp']
+
+for i in range(0,num_lat):
+    for j in range(0,num_long):
+        map[i][j] /= npts[i][j]
+
 
 print(npts)
 print(map)
