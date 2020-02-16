73481d47b7d77af19d449892af3f1aac0fb7b27b
Kendra <kendra.herweck13@gmail.com>
2020-02-16 15:37:27 -0500

    made heatmap overly into a function

--- src/heatmapoverlay.py
+++ src/heatmapoverlay.py
@@ -1,31 +1,21 @@
 import matplotlib.pyplot as plt
 import temp_map as t
-import fishydata as f
 import reduce_data as r
 
-year=2070
-#r.clean(1900)
-#t.temp_arr(zoom_x, zoom_y, map_file='../processed_data/tmap.csv')
-#t.change_temp(t.temp_from_year(2070))
+def overlay_vis(year, fish_type, fish_temp, zoom_x, zoom_y):
 
-zoom_x=2
-zoom_y=4
+    #r.clean(1900)
+    #t.temp_arr(zoom_x, zoom_y, map_file='../processed_data/tmap.csv')
+    t.change_temp(year, zoom_x, zoom_y)
 
-mack_temp = (f.get_fish_temp(0) + f.get_fish_temp(1))/2
-herr_temp = f.get_fish_temp(2)
 
-fish='Mackerel'
-#fish='Herring'
-fish_temp=mack_temp
-#fish_temp=herr_temp
-
-ax = plt.axes()
-heat_map = t.temp_vis(zoom_x, zoom_y, year, map_file='../processed_data/future_tmap.csv', alpha=0.5)
-fish_map = t.fish_vis(fish_temp, 0.3,zoom_x, zoom_y, fish, year, map_file='../processed_data/future_tmap.csv', cmap='Greys')
-ax.set_title('{} Location/Temperature in {}'.format(fish, year))
-plt.xlabel('Longitude')
-plt.ylabel('Latitude')
-plt.show()
+    ax = plt.axes()
+    heat_map = t.temp_vis(zoom_x, zoom_y, year, map_file='../processed_data/future_tmap.csv', alpha=0.5)
+    fish_map = t.fish_vis(fish_temp, 0.3,zoom_x, zoom_y, fish_type, year, map_file='../processed_data/future_tmap.csv', cmap='Greys')
+    ax.set_title('{} Location/Temperature in {}'.format(fish_type, year))
+    plt.xlabel('Longitude')
+    plt.ylabel('Latitude')
+    plt.show()
 
 
 
