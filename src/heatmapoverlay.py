c887fa30c880dcb9f53368c7a8ace11141a3ca10
Kendra <kendra.herweck13@gmail.com>
2020-02-16 12:15:48 -0500

    fixed heat map overlay

--- src/heatmapoverlay.py
+++ src/heatmapoverlay.py
@@ -1,22 +1,14 @@
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 from matplotlib.pyplot import show
-import seaborn as sb
-import numpy as np
 import temp_map as t
-
-zoom_x=1
-zoom_y=2
-map_file='../processed_data/tmap.csv'
-num_file='..processed_data/'
-tmap = np.genfromtxt(map_file, delimiter=',')
-sb.set(font_scale=1)
-binsx = np.linspace(-20, 9, 30 * zoom_x)
-binsx = binsx.astype(int)
-binsy = np.linspace(69, 50, 20 * zoom_y)
-binsy = binsy.astype(int)
+import fishydata as f
 
 
-fish_map = t.fish_vis()
-heat_map = sb.heatmap(tmap, xticklabels=binsx, yticklabels=binsy, cmap='coolwarm',alpha=0.75,zorder=2)
+mack_temp = (f.get_fish_temp(0) + f.get_fish_temp(1))/2
+herr_temp = f.get_fish_temp(2)
+
+heat_map = t.temp_vis(5, 10, map_file='../processed_data/future_tmap.csv')
+fish_map = t.fish_vis(mack_temp, 0.3, 1, 2, map_file='../processed_data/future_tmap.csv')
+show()
+
+
 
